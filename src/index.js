1235872a2eaf3fa86da6b23a578207eed3187434
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-10 02:54:05 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const prompt = require('prompt');
+const prompt = require('prompt')
 
-const count = 1;
+const count = 1
 
 const prompt_attributes = [{
   name: 'githubUser',
-}];
+}]
 
 prompt.get(prompt_attributes, (err, result) => {
   if (err) {
@@ -12,6 +12,6 @@ prompt.get(prompt_attributes, (err, result) => {
     return 1;
   }
   console.log('Command-line received data:');
-});
+})
 
-prompt.start();
+prompt.start()

9be06f9844b01d357966a54e67f04b6687cc2cdf
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-10 02:54:48 -0500

    Update getDataFromGithub.js

--- src/utils/getDataFromGithub.js
+++ src/utils/getDataFromGithub.js
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer');
-
 const getDataFromGithub = async (githubUser) => {
   console.log('Launch Puppeteer');
   const browser = await puppeteer.launch();
@@ -9,6 +8,6 @@ const getDataFromGithub = async (githubUser) => {
   await page.screenshot({ path: `src/images/${getTime()}-${githubUser}.png` });
 
   const githubCounter = await page.evaluate(() => document.getElementsByClassName('Counter')[0].innerText);
-  const githubUserPhoto = await page.evaluate(() => document.getElementsByClassName('avatar-before-user-status')[0].src);
+  const githubUserPhoto await page.evaluate(() => document.getElementsByClassName('avatar-before-user-status')[0].src);
   await browser.close();
 };

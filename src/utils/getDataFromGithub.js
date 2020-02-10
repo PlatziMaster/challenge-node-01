a13e7bc3ed002fef1f2550047fe2373382633a96
Jefferson Aguilar <jefferson@alunices.com>
2020-02-10 12:51:15 -0500

    Solución al reto

--- src/utils/getDataFromGithub.js
+++ src/utils/getDataFromGithub.js
@@ -1,13 +1,18 @@
 const puppeteer = require('puppeteer');
+const config = require('../utils/config');
+const getTime = require('./getTime');
+const slackHook = require('./postToSlack');
+
 const getDataFromGithub = async (githubUser) => {
-  console.log('Launch Puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
-  await page.goto(`${githubUrl}${githubUser}`);
-  await page.screenshot({ path: `src/images/${getTime()}-${githubUser}.png` });
-
+  const githubUrl = config.githubUrl();
+  await page.goto(`${githubUrl}/${githubUser}`);
+  await page.screenshot({ path: `src/images/${getTime.getTime()}-${githubUser}.png` });
+  const githubUserPhoto = await page.evaluate(() => document.getElementsByClassName('avatar-before-user-status')[0].src);
   const githubCounter = await page.evaluate(() => document.getElementsByClassName('Counter')[0].innerText);
-  const githubUserPhoto await page.evaluate(() => document.getElementsByClassName('avatar-before-user-status')[0].src);
+  slackHook.postToSlack(githubUser,githubUserPhoto, githubCounter);
   await browser.close();
 };
+
+exports.getDataFromGithub = getDataFromGithub;

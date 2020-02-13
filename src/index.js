1dba0886a0228b7bfdce62ca97dd541e2b70c665
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2020-02-12 20:21:34 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-const prompt = require('prompt')
+const prompt = require('prompt');
+const getDataFromGithub = require('./utils/getDataFromGithub');
 
-const count = 1
+const count = 1;
 
 const prompt_attributes = [{
   name: 'githubUser',
-}]
+}];
 
-prompt.get(prompt_attributes, (err, result) => {
+prompt.get(prompt_attributes, async (err, result) => {
   if (err) {
     console.log(err);
     return 1;
   }
-  console.log('Command-line received data:');
-})
+  const user = result.githubUser;
+  if (user) {
+    try {
+      console.log('Command-line received data:');
+      console.log('  githubUser:  ${user}');  
+      console.log( await getDataFromGithub(user));
+    }catch(error) {
+      console.log(error);
+    }
+  }
+});
 
-prompt.start()
+prompt.start();
--- src/utils/getDataFromGithub.js
+++ src/utils/getDataFromGithub.js
@@ -1,13 +1,19 @@
 const puppeteer = require('puppeteer');
+const githubUrl = 'https://github.com/';
+const getTime = require('./getTime');
+const postToSlack = require('./postToSlack');
+
 const getDataFromGithub = async (githubUser) => {
   console.log('Launch Puppeteer');
-  const browser = await puppeteer.launch();
+  console.log(getTime());
+  const browser = await puppeteer.launch({ignoreDefaultArgs: ['--disable-extensions']});
   const page = await browser.newPage();
 
   await page.goto(`${githubUrl}${githubUser}`);
   await page.screenshot({ path: `src/images/${getTime()}-${githubUser}.png` });
 
   const githubCounter = await page.evaluate(() => document.getElementsByClassName('Counter')[0].innerText);
-  const githubUserPhoto await page.evaluate(() => document.getElementsByClassName('avatar-before-user-status')[0].src);
+  const githubUserPhoto = await page.evaluate(() => document.getElementsByClassName('avatar-before-user-status')[0].src);
   await browser.close();
 };
+module.exports = getDataFromGithub;

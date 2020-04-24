310f546dc76d811687f9b01116df365fc914e6fd
César Galindo <csgalindos@hotmail.com>
2020-04-23 23:27:03 -0500

    [fix] postToSlack

--- src/utils/getDataFromGithub.js
+++ src/utils/getDataFromGithub.js
@@ -1,15 +1,17 @@
 const puppeteer = require('puppeteer');
 const getData = require('./getTime');
+const postToSlack = require('./postToSlack');
 
 const getDataFromGithub = async (githubUser) => {
   console.log('Launch Puppeteer');
-  const githubUrl = config.github;
+  const githubUrl = config.github();
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.goto(`${githubUrl}${githubUser}`);
   await page.screenshot({ path: `src/images/${getTime()}-${githubUser}.png` });
 
+  
   const githubCounter = await page.evaluate(() => document.getElementsByClassName('Counter')[0].innerText);
   const githubUserPhoto = await page.evaluate(() => document.getElementsByClassName('avatar-before-user-status')[0].src);
   await browser.close();

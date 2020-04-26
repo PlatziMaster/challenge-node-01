0b91eb72d0171b4c898f7e8a7e9aa0048950a276
Tonalli López <aguilablanca_7@live.com>
2020-04-26 17:06:02 -0500

    Challenge completed

--- src/utils/getDataFromGithub.js
+++ src/utils/getDataFromGithub.js
@@ -1,13 +1,23 @@
 const puppeteer = require('puppeteer');
+const getTime = require('./getTime');
+const postToSlack = require('./postToSlack');
+
 const getDataFromGithub = async (githubUser) => {
-  console.log('Launch Puppeteer');
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  try {
+    const githubUrl = 'https://github.com/';
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  await page.goto(`${githubUrl}${githubUser}`);
-  await page.screenshot({ path: `src/images/${getTime()}-${githubUser}.png` });
+    await page.goto(`${githubUrl}${githubUser}`);
+    await page.screenshot({ path: `src/images/${getTime()}-${githubUser}.png` });
 
-  const githubCounter = await page.evaluate(() => document.getElementsByClassName('Counter')[0].innerText);
-  const githubUserPhoto await page.evaluate(() => document.getElementsByClassName('avatar-before-user-status')[0].src);
-  await browser.close();
+    const githubCounter = await page.evaluate(() => document.getElementsByClassName('Counter')[0].innerText);
+    const githubUserPhoto = await page.evaluate(() => document.getElementsByClassName('avatar-before-user-status')[0].src);
+    postToSlack(githubUser, githubUserPhoto, githubCounter);
+    await browser.close();
+  } catch (error) {
+    return error;
+  }
 };
+
+module.exports = getDataFromGithub;
--- src/utils/postToSlack.js
+++ src/utils/postToSlack.js
@@ -1,29 +1,41 @@
+require('dotenv').config();
+const fs = require('fs');
 const fetch = require('node-fetch');
 
+const webhookURL = `${process.env.HOOK}${process.env.TOKEN}`;
+
 const postToSlack = async (user, photo, count) => {
-  const data = JSON.stringify({
-    'blocks': [
-      {
-        'type': 'section',
-        'text': {
-          'type': 'mrkdwn',
-          'text': `*Reto Cumplido* \n https://github.com/${user} \n Numero de repositorios: ${count.trimStart()}`,
+  try {
+    const data = JSON.stringify({
+      'blocks': [
+        {
+          'type': 'section',
+          'text': {
+            'type': 'mrkdwn',
+            'text': `*Reto Cumplido* \n https://github.com/${user} \n Numero de repositorios: ${count.trimStart()}`,
+          },
+          'accessory': {
+            'type': 'image',
+            'image_url': photo,
+            'alt_text': user,
+          },
         },
-        'accessory': {
-          'type': 'image',
-          'image_url': photo,
-          'alt_text': user,
-        },
+      ],
+    });
+    await fetch(webhookURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
       },
-    ],
-  });
-  await fetch(webhookURL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: data,
-  }).then((response) => {
-    console.log(response.size);
-  });
+      body: data,
+    }).then((response) => {
+      fs.writeFile('postToSlackResponse.txt', response.size, (err) => {
+        if (err) throw err;
+      });
+    });
+  } catch (error) {
+    return error;
+  }
 };
+
+module.exports = postToSlack;

edfe6571f3e7214468e4fce5f2b5c5a1fdbfc5db
Héctor Martínez Reséndiz <hectormartinezresendiz@gmail.com>
2020-05-10 20:54:06 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-const prompt = require('prompt')
+const prompt = require('prompt');
+const getDataFromGithub = require('./utils/getDataFromGithub');
 
-const count = 1
+const count = 1;
 
-const prompt_attributes = [{
-  name: 'githubUser',
-}]
+const prompt_attributes = [
+  {
+    name: 'githubUser',
+  },
+];
 
 prompt.get(prompt_attributes, (err, result) => {
   if (err) {
     console.log(err);
     return 1;
   }
+  console.log('Resultados: ' + result);
   console.log('Command-line received data:');
-})
+});
 
-prompt.start()
+prompt.start();
--- src/utils/getDataFromGithub.js
+++ src/utils/getDataFromGithub.js
@@ -7,7 +7,11 @@ const getDataFromGithub = async (githubUser) => {
   await page.goto(`${githubUrl}${githubUser}`);
   await page.screenshot({ path: `src/images/${getTime()}-${githubUser}.png` });
 
-  const githubCounter = await page.evaluate(() => document.getElementsByClassName('Counter')[0].innerText);
-  const githubUserPhoto await page.evaluate(() => document.getElementsByClassName('avatar-before-user-status')[0].src);
+  const githubCounter = await page.evaluate(
+    () => document.getElementsByClassName('Counter')[0].innerText
+  );
+  const githubUserPhoto = await page.evaluate(
+    () => document.getElementsByClassName('avatar-before-user-status')[0].src
+  );
   await browser.close();
 };

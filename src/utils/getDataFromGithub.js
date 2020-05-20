a25242a3dcd1a61dcd883582b718492fcb8be425
SantiagoDuque1700 <duquemoreno12@gmail.com>
2020-05-19 21:15:14 -0500

    retos finalizados

--- src/utils/getDataFromGithub.js
+++ src/utils/getDataFromGithub.js
@@ -1,13 +1,24 @@
 const puppeteer = require('puppeteer');
+const getTime = require('./getTime');
+const postToSlack = require('./postToSlack');
+
+const githubUrl = 'https://github.com/';
+
 const getDataFromGithub = async (githubUser) => {
-  console.log('Launch Puppeteer');
-  const browser = await puppeteer.launch();
+
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   await page.goto(`${githubUrl}${githubUser}`);
   await page.screenshot({ path: `src/images/${getTime()}-${githubUser}.png` });
 
   const githubCounter = await page.evaluate(() => document.getElementsByClassName('Counter')[0].innerText);
-  const githubUserPhoto await page.evaluate(() => document.getElementsByClassName('avatar-before-user-status')[0].src);
+  /*se cambio la clase de avatar-before-user-status a avatar width-full height-full
+  ya que con la original generaba un error al no encontrar el elemento*/
+  const githubUserPhoto = await page.evaluate(() => document.getElementsByClassName('avatar width-full height-full')[0].src);
+
+  postToSlack(githubUser, githubUserPhoto, githubCounter);
   await browser.close();
 };
+
+module.exports = getDataFromGithub;
---
+++ src/utils/getTime.js
@@ -0,0 +1,5 @@
+function getTime() {
+  return new Date().getTime();
+}
+
+module.exports = getTime;

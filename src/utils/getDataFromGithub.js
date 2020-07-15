0155f7391d502802e9f39abb9ab43bd2307dfe25
Juan Espitia <juanespitia92@hotmail.com>
2020-07-14 21:41:46 -0500

    falta cuadrar slack

--- src/utils/getDataFromGithub.js
+++ src/utils/getDataFromGithub.js
@@ -1,13 +1,27 @@
+
 const puppeteer = require('puppeteer');
+
+const githubUrl = 'https://github.com/';
+
+const slack = require('./postToSlack');
+
+const todayDate = new Date();
+
 const getDataFromGithub = async (githubUser) => {
-  console.log('Launch Puppeteer');
+  //console.log(`${githubUrl}${githubUser}`);
+  //console.log('Launch Puppeteer');
+
   const browser = await puppeteer.launch();
+
   const page = await browser.newPage();
-
   await page.goto(`${githubUrl}${githubUser}`);
-  await page.screenshot({ path: `src/images/${getTime()}-${githubUser}.png` });
+  await page.screenshot({ path: `src/images/${todayDate.getTime()}-${githubUser}.png` });
 
-  const githubCounter = await page.evaluate(() => document.getElementsByClassName('Counter')[0].innerText);
-  const githubUserPhoto await page.evaluate(() => document.getElementsByClassName('avatar-before-user-status')[0].src);
+  const githubCounter = await page.evaluate(() => document.getElementsByClassName('Counter').innerText);
+  const githubUserPhoto = await page.evaluate(() => document.getElementsByClassName('avatar-user').src);
+
+  slack(githubUser, githubUserPhoto, githubCounter);
+
   await browser.close();
 };
+module.exports = getDataFromGithub;
